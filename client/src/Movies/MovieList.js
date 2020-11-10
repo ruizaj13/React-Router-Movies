d906b023e3de5e1a5af08fbe8316bed4deaf438d
ruizaj13 <ruizaj13@gmail.com>
2020-11-09 16:15:15 -0800

    mvp met

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default function MovieList(props) {
   return (
@@ -10,11 +11,19 @@ export default function MovieList(props) {
   );
 }
 
+
+
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+
+  const history = useHistory();
+
+  const clickFunc = () =>{
+      history.push(`/movies/${id}`)
+  }
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={clickFunc}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
